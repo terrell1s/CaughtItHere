7c2ae0935339eb11c4ceee8de1f743757e7bfe36
Terrell Springer <twspringer@gmail.com>
2014-08-05 09:32:34 -0400

    Created site

---
+++ CaughtItHere/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CaughtItHere.MvcApplication" Language="C#" %>
---
+++ CaughtItHere/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CaughtItHere.Startup))]
+namespace CaughtItHere
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
